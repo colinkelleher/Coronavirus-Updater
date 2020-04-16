96611f2f185f9645ff229c647aa28c3cd91f87f3
Colin Kelleher <43775947+colinkelleher@users.noreply.github.com>
2020-04-16 19:40:20 +0100

    Updating TIME email is sent in Main
    Currently, multiple emails are sent at the appropriate time, adding a seconds field will reduce the number of emails sent

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import send_email
 
 def daily_email():
     now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    if current_time == "20:10":
+    current_time = now.strftime("%H:%M:%S")
+    if current_time == "20:10:01":
         send_email.send_email()
 
 if __name__ == '__main__':
