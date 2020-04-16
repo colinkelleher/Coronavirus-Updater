10be4194936dc5e4b655bd1996cccd05c7cb32db
Colin Kelleher <43775947+colinkelleher@users.noreply.github.com>
2020-04-16 18:19:42 +0100

    Adding Total Cases, Total Deaths & Total Recovered

--- src/scraper.py
+++ src/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import time
 
 def setup():
-    URL = 'https://www.worldometers.info/coronavirus/'
+    URL = 'https://www.worldometers.info/coronavirus/country/ireland/'
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
@@ -12,5 +12,16 @@ def corona_total_count():
     soup = setup()
     results = soup.findAll("div", class_="maincounter-number")
     corona_total_count = results[0].find('span')
-
     return corona_total_count.text
+
+def corona_total_count_deaths():
+    soup = setup()
+    results = soup.findAll("div", class_="maincounter-number")
+    corona_total_count_deaths = results[1].find('span')
+    return corona_total_count_deaths.text
+
+def corona_total_count_recovered():
+    soup = setup()
+    results = soup.findAll("div", class_="maincounter-number")
+    corona_total_count_recovered = results[2].find('span')
+    return corona_total_count_recovered.text
